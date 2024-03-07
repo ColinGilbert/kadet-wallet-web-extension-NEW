3440f10e482644c91b544783058336825a2cab29
Colin Gilbert <colingilbert86@gmail.com>
2024-03-06 20:30:28 -0600

    Update packages

--- bootstrap-vagrant.sh
+++ bootstrap-vagrant.sh
@@ -4,4 +4,7 @@ apt update
 apt upgrade -y
 apt install docker.io -y
 apt install docker-compose -y
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - &&\
+sudo apt-get install -y nodejs
+apt install npm -y
 docker pull kadena/devnet
